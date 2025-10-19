f84051d7ecde51f47dad65ca52b50d2094c9087d
KingJamal_III <154438134+User-bf@users.noreply.github.com>
2025-10-19 15:10:57 -0400

    new classes and folders + some drivetrain starting code from last year

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/subsystem/WhiskAuto.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.auto.subsystem;
+
+public class WhiskAuto {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/subsystem/Whisk.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.teleop.subsystem;
+
+public class Whisk {
+}
